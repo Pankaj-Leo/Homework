8c6722c77a70394c81b6434cd23982929b056a50
pankaj-babban <124651729+pankaj-babban@users.noreply.github.com>
2023-03-22 16:31:17 +0530

    practise
    practise

---
+++ BillsBurgerChallenge/src/Main.java
@@ -0,0 +1,91 @@
+
+
+ import java.util.Arrays;
+import java.util.Random;
+
+/*
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Item coke = new Item("drink", "coke", 1.50);
+//        coke.printItem();
+//        coke.setSize("LARGE");
+//        coke.printItem();
+//
+//        Item avocado = new Item("Topping", "avocado", 1.50);
+//        avocado.printItem();
+
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder regularMeal = new MealOrder();
+//        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        regularMeal.setDrinkSize("LARGE");
+//        regularMeal.printItemizedList();
+
+//        MealOrder secondMeal = new MealOrder("turkey", "7-up",
+//                "chili");
+//        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up",
+                "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE",
+                "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+    }
+}
+*/
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*int[] firstArray = getRandomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
+
+        int[] secondArray = new int[10];
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.fill(secondArray, 5);
+        System.out.println(Arrays.toString(secondArray));*/
+
+        int[] thirdArray = getRandomArray(10);
+        System.out.println(Arrays.toString(thirdArray));
+         Arrays.sort(thirdArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(thirdArray));
+
+       /* int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
+        int[] smallerArray = Arrays.copyOf(thirdArray, 5);
+        System.out.println(Arrays.toString(smallerArray));
+
+        int[] largerArray = Arrays.copyOf(thirdArray, 15);
+        System.out.println(Arrays.toString(largerArray));*/
+    }
+
+    private static int[] getRandomArray(int len) {
+
+        Random random = new Random();
+        int[] newInt = new int[len];
+        for (int i = 0; i < len; i++) {
+            newInt[i] = random.nextInt(100);
+        }
+
+        return newInt;
+    }
+}
